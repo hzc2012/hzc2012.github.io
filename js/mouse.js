3b0fa39562cc4ac4d2c836f8c1e0742d10bdfe3b
黄泽成 <kk_2012@sina.cn>
2018-03-18 23:05:34 +0800

    Create mouse.js

---
+++ js/mouse.js
@@ -0,0 +1,9 @@
+function showDiv() {
+    var div = document.getElementById('bookmark');
+    if (div.style.display != 'none') {
+		div.style.display = 'none';
+    }
+    else {
+		div.style.display = 'block';
+    }
+};
